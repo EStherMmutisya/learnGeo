e9992ff8499bbf64163f42ddc680ba6dec39d8cd
EStherMmutisya <starjacob994@gmail.com>
2024-08-22 20:54:48 +0300

    added a relationship for rocks and oceans

--- components/rocks.py
+++ components/rocks.py
@@ -1,1 +1,18 @@
-th
+from sqlalchemy import Column, Integer, String, ForeignKey, relationship
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from .ocean import Ocean  # Import the Ocean model
+
+Base = declarative_base()
+
+class Rock(Base):
+    __tablename__ = 'rocks'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    ocean_id = Column(Integer, ForeignKey('oceans.id'))
+    ocean = relationship("Ocean", backref="rocks")
+
+    rivers = relationship("River", secondary=lambda: RiverRock.__table__)
