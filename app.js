9990550764c99a8d7832163f1ca98f03634d167d
EStherMmutisya <starjacob994@gmail.com>
2024-08-26 23:52:54 +0300

    modified the js code to add functionalities

--- app.js
+++ app.js
@@ -1,40 +1,11 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+const baseUrl = 'http://localhost:5000/api/data'; // Replace with your API endpoint URL
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-db = SQLAlchemy(app)
-
-class Data(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
-
-    def __repr__(self)
-        return f'<Data {self.id}>'
-
-@app.route('/api/data', methods=['GET', 'POST'])
-def data():
-    if request.method == 'GET':
-        data = Data.query.all()
-        return jsonify([item.json() for item in data])
-    elif request.method == 'POST':
-        data = Data(name=request.json['name'])
-        db.session.add(data)
-        db.session.commit()
-        return jsonify(data.json())
-
-@app.route('/api/data/<int:id>', methods=['PUT', 'DELETE'])
-def data_update_delete(id):
-    data = Data.query.get_or_404(id)
-    if request.method == 'PUT':
-        data.name = request.json['name']
-        db.session.commit()
-        return jsonify(data.json())
-    elif request.method == 'DELETE':
-        db.session.delete(data)
-        db.session.commit()
-        return jsonify({'message': 'Data deleted successfully'})
-
-if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True)
+function fetchData() {
+  fetch(baseUrl)
+    .then(response => response.json())
+    .then(data => {
+      // Process and display retrieved data
+      console.log(data);
+      // Update your UI or DOM elements with the received data
+    })
+    
